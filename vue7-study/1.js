25206562ea8d9c04550b4db4b54143e2a54b1258
ckk <ckk@163.com>
2017-09-15 08:19:50 +0800

    更新js之间引用学习知识

---
+++ vue7-study/1.js
@@ -0,0 +1,7 @@
+// 引入2.js 赋值给变量xxx
+//import xxx from './2.js'
+
+let age = require('./2.js')
+// console.log(age)
+
+age('jfdskfjkfsaj')
---
+++ vue7-study/2.js
@@ -0,0 +1,7 @@
+// 用来告诉   别的文件可以使用 xxx
+//export default {}
+
+let age = 18;
+module.exports = function (chanshu) {
+    console.log(chanshu)
+}
